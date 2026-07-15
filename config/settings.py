838baeadaa156252e0efa095b14f87b8ac89d05f
KCM-13 <rlacoals4875@gmail.com>
2026-07-15 18:04:44 +0900

    Move SECRET_KEY to .env

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@4+7a$yt@x-r*3z04$7u(4y2dc1h5%n*@e%e3$j*018hp=d_##'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
